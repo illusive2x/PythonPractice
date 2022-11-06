babdd17b5b8e59c28ee673f4413b664e166e2f98
illusive2x <gcarrilloit@gmail.com>
2022-11-05 23:48:14 -0400

    added comments to the program

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -7,28 +7,29 @@ between the user and the computer.
 
 import random
 
+# function to generate the computers choice
 def computer_choice():
     choice = random.choice(['r', 'p', 's'])
     return choice
 
-
+# function to print the programs main menu
 def main_menu():
     print("LET'S PLAY ROCK PAPER SCISSOR\n")
     print("Enter 'r' for rock \nEnter 'p' for paper \nEnter 's' for scissors\n")
 
-
+# functiont to get the user's choice
 def user_input():
     user = input("Enter you choice: ")
     return user
 
-
+# function to start the game
 def play():
     main_menu()
     choice = computer_choice()
     user = user_input()
     winner(user, choice)
 
-
+# function to determine the winner
 def winner(player, opponent):
     if player == opponent:
         print("\nIt's a tie!")
@@ -40,5 +41,5 @@ def winner(player, opponent):
         print("\nYou lose!")
         print(f"Computer: {opponent} \nUser: {player}")
 
-
-play()
+if __name__ == '__main__':
+    play()
